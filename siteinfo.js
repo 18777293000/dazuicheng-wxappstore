8c075e29af7fe5384d4aca9cc0e4936b57cc612a
18777293000 <18777293000@163.com>
2020-01-15 09:36:48 +0800

    first commit

---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "大嘴成土特产商城",
+    uniacid: "7",
+    acid: "7",
+    multiid: "0",
+    version: "1.18",
+    siteroot: "https://wakakago.510show.com/app/index.php",
+    design_method: "3"
+};
